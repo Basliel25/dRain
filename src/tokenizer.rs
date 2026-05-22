df5522d1aaf83699f72df7f9c349d48063447f5a
Ras Bas <basgug25@gmail.com>
2026-05-22 14:46:30 +0200

    Added regex patterns

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,12 +1,18 @@
-/// Removing already known patterns by placeholders,
-/// conserves tree entropy, since every known will 
-/// eventually be crating a new branch, remove known
-/// placeholders with a placeholder
-/// A wildcard placeholder for repeated entries
-/// PIDS
-/// IPs
-///
-///This is going to be done using regex matching
+// Removing already known patterns by placeholders,
+// conserves tree entropy, since every known will 
+// eventually be crating a new branch, remove known
+// placeholders with a placeholder
+// A wildcard placeholder for repeated entries
+// PIDS
+// IPs
+
+use std::sync::LazyLock;
+use regex::Regex;
+
+let RE_IP = Regex::new(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b").unwrap();
+let RE_HEX = Regex::new(r"\b0x[0-9a-fA-F]+\b").unwrap();
+let RE_INT = Regex::new(r"\b\d+\b").unwrap();
+
 const WILDCARD: &str = "<*>";
 
 /// Preprocess
