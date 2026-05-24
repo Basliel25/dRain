c68a2cab2d38bfef6fa8ef45be6cb01ac252fe76
Ras Bas <basgug25@gmail.com>
2026-05-24 12:46:44 +0200

    Setup data structures

--- src/tree.rs
+++ src/tree.rs
@@ -1,10 +1,23 @@
 use std::collections::HashMap;
 use crate::template::Template;
 
-pub sruct Tree {}
+pub struct Tree {
+    by_length: HashMap<usize, TreeNode>,
+    next_id: u64,
+    threshold: f64,
+}
 
-enum TreeNode {}
+enum TreeNode {
+    Length(HashMap<Box<str>, TreeNode>),
+    Leaf(Vec<Template>),
+}
 
-pub struct MatchOutcome {}
+pub struct MatchOutcome {
+    pub id: u64,
+    pub params: Vec<String>,
+    pub created: bool,
+}
+
+
 
 
