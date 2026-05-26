b46930a83390d79d5744f93eb82f2cfa05a4e3aa
Ras Bas <basgug25@gmail.com>
2026-05-26 15:47:50 +0200

    Added cli support for three arguments Arguments - `"-h" | "--help"`: displays usage help - `"-s" | "--save" "<path>"`: saves to a json file - `"-l" | "--load" "<path>"`: Loads from a json file
    Tree now will be initalized to the contents of the json file if loaded,
    empty tree if no path is provided

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,71 @@
-use std::io::{self, BufRead};
+use std::env;
+use std::io::{self, BufRead, Write};
+use std::fs;
 use dRain::tree::Tree;
 use dRain::tokenizer::{preprocess, tokenize_line};
+use dRain::snapshot::DrainSnapshot;
 
-fn main() {
-    let mut tree = Tree::new(0.5);
+fn main()-> std::process::ExitCode {
+
+    let args: Vec<String> = env::args().collect();
+    let mut load_path: Option<String> = None;
+    let mut save_path: Option<String> = None;
+
+    let mut i = 1;
+    while i < args.len() {
+        match args[i].as_str(){
+            "--load" | "-l" => {
+                let Some(p) = args.get(i+1) else {
+                    eprintln!("error: load requires a path");
+                    return std::process::ExitCode::from(2);
+                };
+                load_path = Some(p.clone());
+                i += 2;
+            },
+
+            "--save" | "-s" => {
+                let Some(p) = args.get(i+1) else {
+                    eprintln!("error: save requires a path");
+                    return std::process::ExitCode::from(2);
+                };
+                save_path = Some(p.clone());
+                i += 2;
+            },
+            "-h" | "--help" => {
+                eprintln!("Usage: drain [--load <path>] | [--save <path>]");
+                return std::process::ExitCode::SUCCESS;
+            }
+
+            other => {
+                eprintln!("Unknown argumet, use -h to see possible arguments");
+                    return std::process::ExitCode::from(2);
+            }
+        }
+    }
+
+    // Build a tree from path or from scratch
+    let mut tree = match load_path {
+        Some(path) => {
+            let json = match fs::read_to_string(&path) {
+                Ok(s) => s,
+                Err(e) => {
+                    eprintln!("error: Error reading file {} \n{}\n", path, e);
+                    return std::process::ExitCode::from(1);
+                }
+            };
+
+            let snap: DrainSnapshot = match serde_json::from_str(&json) {
+                Ok(s) => s, 
+                Err(e) => {
+                    eprintln!("error: Error reading file {} \n{}\n", path, e);
+                    return std::process::ExitCode::from(1);
+                }
+            };
+            Tree::load(snap)
+        },
+        None => Tree::new(0.5)
+
+    };
     let stdin = io::stdin();
 
     for line in stdin.lock().lines() {
@@ -23,6 +85,7 @@ fn main() {
         }
         println!();
     }
+    std::process::ExitCode::SUCCESS
 }
 
 
